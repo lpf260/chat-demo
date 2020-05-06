88430293feb6c366ade549607d4be4602ba24607
pengfei.liu <pengfei.liu@vcg.com>
2020-05-06 19:15:14 +0800

    first commit

---
+++ index.js
@@ -0,0 +1,20 @@
+var app = require("express")();
+var http = require("http").createServer(app);
+var io = require("socket.io")(http);
+
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/index.html");
+});
+
+io.on("connect", (socket) => {
+  socket.on("chat message", (msg) => {
+    io.emit("chat message", msg);
+  });
+  socket.on("disconnect", () => {
+    console.log("user disconnected");
+  });
+});
+
+http.listen(3000, () => {
+  console.log("listening on *:3000");
+});
